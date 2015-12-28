26d06531170751c9974c39818fbd2e7e1bf6169e
Yanai Klugman <Yanaik9541@gmail.com>
2015-12-28 16:48:41 +0200

    Update 4:33

--- Js/board.js
+++ Js/board.js
@@ -5,22 +5,50 @@
 $board = [];
 
 (function createBoard(sx, sy) {
-    for (var i=0; i<sx; i++) {
+    var tempJ, tempI;
+
+    for (var i = 0; i < sy; i++) {
+        tempI = i;
         $board.push([]);
         var $row = $('<div></div>');
         $row.addClass('row');
-        for (var j=0; j<sy; j++) {
+        for (var j = 0; j < sx; j++) {
+            tempJ = j;
             var $tile = $('<div></div>');
             $tile.addClass('tile');
-            if (j%2) {
+            if (j % 2) {
                 $tile.addClass('sky');
             } else {
                 $tile.addClass('cloud');
             }
+
+            /*WorkSpace - Yanai*/
+            $tile.on('click', clickTile.bind(this, tempI, tempJ));
+            $tile.on('mousedown', checkTile.bind(this, tempI, tempJ));
+            //$tile.bind('click mousedown', {
+                //click: clickTile(this, tempI, tempJ),
+                //mousedown: checkTile(this, tempI, tempJ)
+            //});
+            /*End WorkSpace - Yanai*/
+
             $row.append($tile);
             $board[i].push($tile);
         }
         $('#board').append($row);
     }
-    $board[1][1].css('background-color','black');
-}(20,32));
+    $board[1][1].css('background-color', 'black');
+}(32, 20));
+
+
+/*WorkSpace - Yanai*/
+function clickTile(x, y) {
+    //console.log($(this).className)
+    console.log($board[y][x]);
+    $board[y][x].toggle('sky');
+    $board[y][x].toggle('cloud');
+}
+
+function checkTile(x, y) {
+    var tile = $board[x][y];
+}
+/*End WorkSpace - Yanai*/
