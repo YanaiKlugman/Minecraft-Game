e197011e9ae0d9b69ecdd4a0e7dc020df81bcf63
Yanai Klugman <Yanaik9541@gmail.com>
2015-12-28 12:04:15 +0200

    Update 12:03

--- Js/board.js
+++ Js/board.js
@@ -1,3 +1,27 @@
 /**
  * Created by User on 12/28/2015.
  */
+
+$board = [];
+
+(function createBoard(sx, sy) {
+    for (var i=0; i<sx; i++) {
+        $board.push([]);
+        var $row = $('<div></div>');
+        $row.addClass('row');
+        console.log($row);
+        for (var j=0; j<sy; j++) {
+            var $tile = $('<div></div>');
+            $tile.addClass('tile');
+            if (j%2) {
+                $tile.addClass('sky');
+            } else {
+                $tile.addClass('cloud');
+            }
+            $row.append($tile);
+            $board[i].push($tile);
+        }
+        $('#board').append($row);
+    }
+    $board[1][1].css('background-color','black');
+}(27,35));
