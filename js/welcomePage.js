ddcabb76955889334c2cbc35e375d8109b5c0b64
Yanai Klugman <Yanaik9541@gmail.com>
2015-12-30 11:58:57 +0200

    Yanai's changes:

--- js/welcomePage.js
+++ js/welcomePage.js
@@ -2,20 +2,22 @@
  * Created by User on 12/29/2015.
  */
 function welcome(){
-    $('.saveload').hide();
+    var $saveload = $('.saveload');
+    var $innerContainer = $('.inner-container');
+    $saveload.hide();
     var $startButton = $('<input>');
     $startButton.attr('type', 'button');
     $startButton.attr('value', "Ready to Play MineCraft?");
     $startButton.addClass('startButton');
     $startButton.on('click', function () {
-        $('.inner-container').fadeIn(1000);
+        $innerContainer.fadeIn(1000);
         gameInit();
-        $('.inner-container').css({'background':'none', 'padding':'0px'});
+        $innerContainer.css({'background':'none', 'padding':'0px'});
         $startButton.hide();
-        $('.saveload').show();
+        $saveload.show();
     });
-    $('.inner-container').append($startButton);
-    setTimeout(function () {
-        $startButton.click();
-    }, 1000);
+    $innerContainer.append($startButton);
+    //setTimeout(function () {
+    //    $startButton.click();
+    //}, 1000);
 }
